da9a1f903bf1ac87354c3115c2eb01fcd7acfd73
esrpmontealegre <esrpmontealegre@gmail.com>
2023-10-05 21:54:42 +0800

    10/5

--- DemoApps/Shopee.Web/Program.cs
+++ DemoApps/Shopee.Web/Program.cs
@@ -3,6 +3,7 @@ using Demo.Shared;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddShopeeContext();
+//controllers,endpointapiexplorer,swaggergenerator instead of razor pages <-for api
 
 var app = builder.Build();
 
@@ -16,6 +17,7 @@ app.UseDefaultFiles();//should come before static files
 app.UseStaticFiles();
 
 app.MapRazorPages();
+//map controllers,endpointapiexplorer,swaggergenerator instead of razor pages <-for api
 
 app.MapGet("/hello", () => "Hello World!");
 
